ffb3d5dc1646ef3ca6e7e1dceb594b7db35adee8
bryophyta <37048459+bryophyta@users.noreply.github.com>
2020-06-29 17:17:03 +0100

    Update drawing-functions.js

--- svg-network-graph/drawing-functions.js
+++ svg-network-graph/drawing-functions.js
@@ -25,7 +25,7 @@ function drawLine(
 				line.setAttributeNS(null, 'x2', end.x);
 				line.setAttributeNS(null, 'y2', end.y);
 				line.setAttributeNS(null, 'stroke', color != '' ? color : primaryDark);
-				line.setAttributeNS(null, 'stroke-width', weight != '' ? weight : 0);
+				line.setAttributeNS(null, 'stroke-width', weight != '' ? weight : 1);
 				canv.appendChild(line);
 			}
 
@@ -76,12 +76,17 @@ function drawCircle(
 
 
 //I had been having the problem that this event kept being called over and over whilst the mouse hovered, even though it never left the initial point. Worked out that this was because I was re-drawing the whole graph, thereby putting a *new* element underneath the cursor, which was then having a mouseenter event triggered.
+//Pretty sure that DOM queries are going to be more expensive than array queries so I should probably check whether the label exists *before* doing getElement... (i.e. don't just get it first then check it's not null...)
 function mouseOverPoint(event){
         let pointId = event.target.id;
         let hoveredNode = document.getElementById(pointId);
         let hoveredLabel = document.getElementById('label' + pointId);
         hoveredNode.setAttributeNS(null, 'stroke', secondaryMid);
-        hoveredLabel.style.display = 'inline-block';
+        if(hoveredLabel != null){
+           hoveredLabel.style.display = 'inline-block';
+        }
+        console.log('hover');
+        console.log(net);
         for (edge of net.edges){
             if (edge.source == pointId){
                 let outEdge = document.getElementById('edge' + edge.source + edge.target);
@@ -90,7 +95,9 @@ function mouseOverPoint(event){
                 outEdge.style.stroke = secondaryMid;
                 outEdge.style['stroke-width'] = 2;
                 outNode.setAttributeNS(null, 'stroke', secondaryMid);
-                outLabel.style.display = 'inline-block';
+                if(outLabel != null){
+                    outLabel.style.display = 'inline-block';
+                }
             } else if(edge.target == pointId){
                 //doing this as else/if means that outgoing links will take precedence, if there is one of each. But I need to add something that could show bidirectional links anyway, and that will probably mean needing to come up with a different structure overall anyway?
                 let inEdge = document.getElementById('edge' + edge.source + edge.target);
@@ -99,18 +106,23 @@ function mouseOverPoint(event){
                 inEdge.style.stroke = tertiaryMid;
                 inEdge.style['stroke-width'] = 2;
                 inNode.setAttributeNS(null, 'stroke', tertiaryMid);
-                inLabel.style.display = 'inline-block';
+                if (inLabel != null){
+                    inLabel.style.display = 'inline-block';
+                }
             }   
         }     
 }
 
 //this basically manually undoes what the mouseenter function did, so will need to be adjusted if the other is...
+//Pretty sure that DOM queries are going to be more expensive than array queries so I should probably check whether the label exists *before* doing getElement... (i.e. don't just get it first then check it's not null...)
 function mouseOutPoint(event){
         let pointId = event.target.id;
         let hoveredNode = document.getElementById(pointId);
         let hoveredLabel = document.getElementById('label' + pointId);
         hoveredNode.setAttributeNS(null, 'stroke', primaryDark);
-        hoveredLabel.style.display = 'none';
+        if (hoveredLabel != null) {
+           hoveredLabel.style.display = 'none';
+        }
         for (edge of net.edges){
             if (edge.source == pointId){
                 let outEdge = document.getElementById('edge' + edge.source + edge.target);
@@ -119,7 +131,9 @@ function mouseOutPoint(event){
                 outEdge.style.stroke = primaryDark;
                 outEdge.style['stroke-width'] = 1;
                 outNode.setAttributeNS(null, 'stroke', primaryDark);
-                outLabel.style.display = 'none';
+                if (outLabel != null){
+                    outLabel.style.display = 'none';
+                }
             } else if(edge.target == pointId){
                 let inEdge = document.getElementById('edge' + edge.source + edge.target);
                 let inNode = document.getElementById(edge.source);
@@ -127,7 +141,9 @@ function mouseOutPoint(event){
                 inEdge.style.stroke = primaryDark;
                 inEdge.style['stroke-width'] = 1;
                 inNode.setAttributeNS(null, 'stroke', primaryDark);
-                inLabel.style.display = 'none';
+                if (inLabel != null){
+                    inLabel.style.display = 'none';
+                }
             }   
         }
 }
